75bafafe615c6c9733d8d597c583f2e29728818c
Rishav Bikarwar <rishav20@iitk.ac.in>
2022-02-18 03:30:31 +0530

    working like a charm

--- Assignment 2/sat_solver.cpp
+++ Assignment 2/sat_solver.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // Global Variables
 int num_vars, num_clauses;
+long long total = 0;
 vector<vector<int>> F;         // Formula
 vector<int> tv_clause;         // Truth Value of each clause
 vector<int> I;                 // Interpretation
@@ -27,21 +28,22 @@ void initialize_variables() {
 
 int simplify_and_check() {
     // simplify and check satisfiability of the formula
-    for (auto cl : adj[I[0]]) {             // I[0] is the last literal specified in I
-        if (tv_clause[abs(cl)] == 0) {      // Check if clause is not satisfied yet
-            if (cl*I[I[0]] > 0) {           // Check if clause gets satisfied
-                tv_clause[abs(cl)] = 1;
-                for (auto l : F[abs(cl)]) {
+    for (auto c : adj[I[0]]) {             // I[0] is the last literal specified in I
+        int ac = abs(c);
+        if (tv_clause[ac] == 0) {      // Check if truth value of clause is not found yet
+            if (c*I[I[0]] > 0) {           // Check if clause gets satisfied
+                tv_clause[ac] = 1;
+                for (auto l : F[ac]) {
                     (l > 0)? cnt[l].first-- : cnt[-l].second--;
                     tcnt[abs(l)]--;
                 }
-                F[abs(cl)].clear();
+                F[ac].clear();
             }
             else {
-                if (F[abs(cl)].size() >= 2) {
+                if (F[ac].size() >= 2) {
                     // remove literal from the clause
-                    F[abs(cl)].erase(remove(F[abs(cl)].begin(), F[abs(cl)].end(), I[0]*(abs(cl)/cl)), F[abs(cl)].end());
-                    (I[0]*cl > 0)? cnt[I[0]].first-- : cnt[I[0]].second--;
+                    F[ac].erase(remove(F[ac].begin(), F[ac].end(), I[0]*(ac/c)), F[ac].end());
+                    (c > 0)? cnt[I[0]].first-- : cnt[I[0]].second--;
                     tcnt[I[0]]--;
                 }
                 else {
@@ -79,33 +81,75 @@ int unit_propagation() {
 }
 
 
+int pure_literal() {
+    for (int i = 1; i <= num_vars; i++) {
+        if (I[i] == 0 && (cnt[i].first == 0 || cnt[i].second == 0) && tcnt[i] > 0) {
+            int sign = cnt[i].first > 0 ? 1 : -1;
+            I[i] = sign;
+            I[0] = i;
+            int res = simplify_and_check();
+            if (res != 0) return res;
+            i = 0;
+        }
+    }
+    return 0;
+}
+
+
+int find_xi() {
+    int xi = 0;
+    long long maxm = 0;
+    for (int i = 1; i <= num_vars; i++) {
+        long long cur = 1024*cnt[i].first*cnt[i].second;
+        cur += cnt[i].first + cnt[i].second;
+        if (cur > maxm) {
+            maxm = cur;
+            xi = i;
+        }
+    }
+    return xi;
+}
+
+
 bool sat() {
     // if ( I ⇒ F ) return true
     // if ( I ⇒ ¬F ) return false
     int res = simplify_and_check();
-    if (res != 0) return res == 1;
+    if (res != 0) return (res == 1);
     
-    // F,I = up(F,I)
-    // if I is inconsistent return false
-    res = unit_propagation();
-    if (res != 0) return res == 1;
     
     // F,I = pure(F,I)
     // if F = ∅ return true
+    res = pure_literal();
+    if (res != 0) return (res == 1);
+
+    // F,I = up(F,I)
+    // if I is inconsistent return false
+    res = unit_propagation();
+    if (res != 0) return (res == 1);
+    
     
     // choose xi that I does not assign
     // if sat(F, I ∪ { xi=true }) return true
     // if sat(F, I ∪ { xi=false }) return true
     // return false.
     vector<vector<int>> F_bckup(F);
-    vector<int> I_bckup(I);
-    int xi = max_element(tcnt.begin(), tcnt.end()) - tcnt.begin();
+    vector<int> I_bckup(I), tcnt_backup(tcnt), tv_clause_backup(tv_clause);
+    vector<pair<int,int>> cnt_bckup(cnt);
+
+    total++;
+    int xi = find_xi();
     I[xi] = 1;      I[0] = xi;
-    if (sat()) return true;
-    F = F_bckup;    I = I_bckup;
-    I[xi] = -1;     I[0] = xi;
-    if (sat()) return true;
+    if (sat()) {total--; return true;}
     
+    F = F_bckup;    I = I_bckup;
+    tv_clause = tv_clause_backup;
+    tcnt = tcnt_backup;
+    cnt = cnt_bckup;
+
+    I[xi] = -1;     I[0] = xi;
+    if (sat()) {total--; return true;}
+    total--;
     return false;
 }
 
@@ -156,30 +200,41 @@ int main() {
         stringstream ss(line);
         int tmp;
         vector<int> clause;
-        while (ss >> tmp) {
-            clause.push_back(tmp);
+        bool valid = false;
+        while (!valid && ss >> tmp) {
             if (tmp == 0) break;
             // if both xi and ~xi are present in the clause, mark the clause as true
             // additionally, add xi only one time in a clause
             if (adj[abs(tmp)].size() != 0 && abs(adj[abs(tmp)].back()) == F.size()) {
-                if (tmp*adj[abs(tmp)].back() < 0)
+                if (tmp*adj[abs(tmp)].back() < 0) {
                     tv_clause[F.size()] = 1;
+                    valid = true;
+                }
                 continue;
             }
+            clause.push_back(tmp);
             (tmp > 0)? cnt[tmp].first++ : cnt[-tmp].second++;
             tcnt[abs(tmp)]++;
             adj[abs(tmp)].push_back(F.size()*(tmp/abs(tmp)));    // Add clause to adjacency list
         }
-        clause.pop_back();
+        if (valid) {
+            for (auto l : clause) {
+                adj[abs(l)].pop_back();
+                (l > 0)? cnt[l].first-- : cnt[-l].second--;
+                tcnt[abs(l)]--;
+            }
+            clause.clear();
+        }
         F.push_back(clause);
         getline(file, line);
     }
     file.close();
 
     vector<vector<int>> F_bckup(F);
-    vector<int> I_bckup(I);
+    vector<int> I_bckup(I), tcnt_backup(tcnt), tv_clause_backup(tv_clause);
+    vector<pair<int,int>> cnt_bckup(cnt);
 
-    int xi = max_element(tcnt.begin(), tcnt.end()) - tcnt.begin();
+    int xi = find_xi();
     I[xi] = 1;
     I[0] = xi;
     
@@ -188,8 +243,10 @@ int main() {
         print_model();
     }
     else {
-        F = F_bckup;
-        I = I_bckup;
+        F = F_bckup;    I = I_bckup;
+        tv_clause = tv_clause_backup;
+        tcnt = tcnt_backup;
+        cnt = cnt_bckup;
         I[xi] = -1;
         I[0] = xi;
         if (sat()) {
